2313a888e141ca2d20c02e847eea5c1d5ae3406e
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2024-05-23 20:32:00 -0500

    exercise-1

--- src/index.js
+++ src/index.js
@@ -4,37 +4,49 @@
  **/
 
 const baseUrl = "https://platzi-avo.vercel.app";
-
+const mountNode = document.getElementById("js-mount");
 //Web API
 // Conectar Server
+const formatPrice = (price) =>
+    new Intl.NumberFormat("en-EN", {
+        style: "currency",
+        currency: "USD",
+    }).format(price);
 
-window.fetch(`${baseUrl}/api/avo`)
-// Procesar Respuesta y Convertir a JSON
-.then((respuesta) => respuesta.json())
-
-// JSON --> Data --> Cargar Navegador
-.then((responseJson) => {
-    const allItems = [];
-    responseJson.data.forEach((item)=>{
+!(async function () {
+    const response = await fetch(`${baseUrl}/api/avo`);
+// More about Scread in (Mozilla > Operators>Spread syntax )
+    const {data: allAvos} = await response.json();
+//HTML Nodes for each avocado from the API
+    const nodeArray = allAvos.map((avocado) => {
         //Crear Imagen
         const image = document.createElement("img");
+       
         // Llamar imágenes
-        image.src = `${baseUrl}${item.image}`;
-        
+        image.src = `${baseUrl}${avocado.image}`;
+        image.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
         // Crear Título
         const title = document.createElement("h2");
         //Etiquetar fruta
-        title.textContent = item.name;
-
+        title.className = "text-lg";
+        title.textContent = avocado.name;
         // Crear Precio
         const price = document.createElement("div");
+        price.className = ("text-gray-600");
         // Llamar Precio
-        price.textContent = item.price;
+        price.textContent = formatPrice(avocado.price);
 
-        const container = document.createElement("div");
-        container.append(image, title, price);
+        const priceAndTitle = document.createElement("div");
+        priceAndTitle.className = "text-center md:text-left";
+        priceAndTitle.appendChild(title);
+        priceAndTitle.appendChild(price);
 
-        allItems.push(container);
+        const card = document.createElement("div");
+        card.className = "md-flex bg-white rounded-lg p-6 hover:bg-gray-300";
+        card.appendChild(image);
+        card.appendChild(priceAndTitle);
+
+        return card;
     });
-    document.body.append(...allItems);
-});
+mountNode.append(...nodeArray);
+})();
